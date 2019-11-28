35192f446858fd245961b38cd7f5935264b4a4df
viviane-scarlatti <viviane.scarlatti@gmail.com>
2019-11-28 15:59:36 -0300

    Exercício JavaScript Jogo

---
+++ jogo.js
@@ -0,0 +1,14 @@
+// jogador 1 = X
+// jogador 2 = O
+
+var vezDoJogador = 1;
+
+function joga(idDiv){
+    var textoInterno = document.getElementById(idDiv).innerHTML;
+    if (textoInterno =="" ) {
+        if(vezDoJogador == 1)
+        {
+            
+        }
+    }
+}
