496261c0f255cd0abe3ecab7d6501cd1b0d195d1
viviane-scarlatti <viviane.scarlatti@gmail.com>
2019-12-02 15:52:34 -0300

    Exercício Javascript

--- listadetarefas.js
+++ listadetarefas.js
@@ -3,12 +3,12 @@ var num=0;
 function novatarefa(){
     num++;
     var nometarefa = document.getElementById("nomedatarefa").value;
-    var novalinha = '<div class="row"><div clas="col-md-2"><input type="checkbox"></div><div class="col-md-8">**titulodaTarefa**</div><div class="col-md-2">**numSequencia**</div></div>';
+    var novalinha='<div class="row"><div class="col-md-2"><input type="checkbox"></div><div class="col-md-8">**tituloDaTarefa**</div><div class="col-md-2">**numSequencia**</div></div>';
 
-    novalinha = novalinha.replace("**numSequencia**", num);
-    novalinha = novalinha.replace("**tituloDaTarefa**", nometarefa);
+    novalinha = novalinha.replace("**numSequencia**",num);
+    novalinha = novalinha.replace("**tituloDaTarefa**",nometarefa);
     console.log(novalinha);
 
     document.getElementById("lista").innerHTML+=novalinha;
-
+    
 }
